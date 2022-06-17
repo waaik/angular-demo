e8b8b26cd6d2af9567eef3f4eaed9939bf84f703
waaik.collier-perrin <waaik.collier-perrin@cognizant.com>
2022-06-17 15:27:59 -0400

    Added shopping list edit

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,7 @@ export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [ 
     new Recipe('Test name', 'Test desc', 'https://th.bing.com/th/id/R.b8f54daefcf18c526df4f98ed371907a?rik=PSzLTrkn%2fApdbQ&riu=http%3a%2f%2fportfolio.cpl.co.uk%2finspire%2f2019-autumn%2frecipes-intro%2fright.jpg&ehk=Z4QR7P7URO7Qh4gZkOmCCdsd0XIYQSFsdbU95LVmESs%3d&risl=&pid=ImgRaw&r=0'),
-    new Recipe('Test name', 'Test desc', 'https://th.bing.com/th/id/R.b8f54daefcf18c526df4f98ed371907a?rik=PSzLTrkn%2fApdbQ&riu=http%3a%2f%2fportfolio.cpl.co.uk%2finspire%2f2019-autumn%2frecipes-intro%2fright.jpg&ehk=Z4QR7P7URO7Qh4gZkOmCCdsd0XIYQSFsdbU95LVmESs%3d&risl=&pid=ImgRaw&r=0')
+    new Recipe('Test name', 'Test desc', 'https://www.tasteofhome.com/wp-content/uploads/2017/10/exps115227_TH153340C10_01_3b.jpg')
   ];
 
   constructor() { }
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, ElementRef, OnInit, Output, ViewChild } from '@angular/core';
+import { EventEmitter } from '@angular/core'
+import { Ingredient } from 'src/app/shared/ingredient';
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
-
+  @ViewChild('amountInput') amountInputRef: ElementRef;
+  @ViewChild('nameInput') nameInputRef: ElementRef;
+  // creating new event listener
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value
+    const ingAMount = this.amountInputRef.nativeElement.value
+    const newIngredient = new Ingredient(ingName, ingAMount);
+    this.ingredientAdded.emit(newIngredient)
   }
 
 }
